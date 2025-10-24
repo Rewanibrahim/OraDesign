321252d2860c10d2605e9d780c11026d846ab11e
Rewan <irewan28@gmail.com>
2025-10-24 03:36:54 +0300

    Fix static files and assets paths

--- public/admin.js
+++ public/admin.js
@@ -931,7 +931,7 @@ async function addTool() {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/tools", {
+    const res = await fetch("${SERVER_URL}/api/tools", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, cost, quantity })
