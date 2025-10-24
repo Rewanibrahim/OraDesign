93598f2d641baf4d74a7e1d49c082fe12dd806c9
Rewan <irewan28@gmail.com>
2025-10-24 03:07:29 +0300

    Add start script

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ app.use("/api/tools", toolRoutes);
 app.get("/", (req,res) => res.send("OraDesign Server is running!"));
 
 // توصيل DB وتشغيل السيرفر
-const PORT = process.env.PORT; // مش ندي قيمة افتراضية
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
