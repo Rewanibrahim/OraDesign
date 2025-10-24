63dbbe90cadf9e253d57330d8cd358c59e5707c9
Rewan <irewan28@gmail.com>
2025-10-24 03:15:28 +0300

    serve admin.html

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/tools", toolRoutes);
 
 // Route رئيسية تفتح صفحة admin.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
+  app.use(express.static(path.join(__dirname, "public")));
 });
 
 // توصيل DB وتشغيل السيرفر
